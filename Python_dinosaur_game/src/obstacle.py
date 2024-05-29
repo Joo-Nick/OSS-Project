7c6064b33631613fc77ca18545db97e0e8bd62e9
cjsks <cjsks765@gmail.com>
2024-05-29 10:00:44 +0900

    [Feat] 익룡 장애물 추가

--- Python_dinosaur_game/src/obstacle.py
+++ Python_dinosaur_game/src/obstacle.py
@@ -5,14 +5,35 @@ class Tree:
         self.screen = screen
         self.image = pygame.image.load(img_path)
         
-        tree_height = self.image.get_size()[1]
+        tree_height = self.image.get_height()
         
         self.x = max_width
         self.y = max_height - tree_height
-        self.speed = 12.0 # 나중에 플레이 시간에 따른 점수 연계해서 속도 변경
+        self.speed = 12
 
     def move(self):
         self.x -= self.speed
+        if self.x <= 0:
+            self.x = 800
+
+    def draw(self):
+        self.screen.blit(self.image, (self.x, self.y))
+        
+class FlyingObstacle:
+    def __init__(self, screen, img_path='Python_dinosaur_game/images/flying_obstacle.png', max_width=800, max_height=400):
+        self.screen = screen
+        self.image = pygame.image.load(img_path)
+        self.x = max_width
+        self.y = 0  # 화면의 상단에서 시작
+        self.speed = 7
+        self.direction = 1  # 지그재그 움직임을 위한 방향 변수
+
+    def move(self):
+        self.x -= self.speed # 가로방향의 속도
+        self.y += self.direction * 12  # 세로방향의 속도
+        if self.y <= 0 or self.y >= (300 - self.image.get_height()):  # 상하 이동 범위 제한
+            self.direction *= -1  # 방향 전환
+
         if self.x <= 0:
             self.x = 800
 
