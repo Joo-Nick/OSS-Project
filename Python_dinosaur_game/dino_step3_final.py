8844e6d0928bd81f853e3f4999dac1e734381075
Joonik Seo <joon980923@naver.com>
2024-05-30 21:54:52 +0900

    [feat] 공룡 기능 추가

--- Python_dinosaur_game/dino_step3_final.py
+++ Python_dinosaur_game/dino_step3_final.py
@@ -3,6 +3,7 @@
 import pygame
 import sys
 from src.obstacle import Tree
+from src.dino import Dino
 
 # step1 : set screen, fps
 # step2 : show dino, jump dino
@@ -18,65 +19,32 @@ def main():
     # set screen, fps
     screen = pygame.display.set_mode((MAX_WIDTH, MAX_HEIGHT))
     fps = pygame.time.Clock()
+    run = True
 
     # dino
-    imgDino1 = pygame.image.load('Python_dinosaur_game/images/dino1.png')
-    imgDino2 = pygame.image.load('Python_dinosaur_game/images/dino2.png')
-    dino_height = imgDino1.get_size()[1]
-    dino_bottom = MAX_HEIGHT - dino_height
-    dino_x = 50
-    dino_y = dino_bottom
-    jump_top = 200
-    leg_swap = True
-    is_bottom = True
-    is_go_up = False
+    dino = Dino()
 
     # tree
     tree = Tree(screen, 'Python_dinosaur_game/images/tree.png', MAX_WIDTH, MAX_HEIGHT)
 
-    while True:
+    while run:
         screen.fill((255, 255, 255))
+        userinput = pygame.key.get_pressed()
 
         # event check
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if is_bottom:
-                    is_go_up = True
-                    is_bottom = False
-
-        # dino move
-        if is_go_up:
-            dino_y -= 10.0
-        elif not is_go_up and not is_bottom:
-            dino_y += 10.0
-
-        # dino top and bottom check
-        if is_go_up and dino_y <= jump_top:
-            is_go_up = False
-
-        if not is_bottom and dino_y >= dino_bottom:
-            is_bottom = True
-            dino_y = dino_bottom
+                run = False
 
         # tree move
         tree.move()
 
-
         # draw tree
         tree.draw()
 
-
         # draw dino
-        if leg_swap:
-            screen.blit(imgDino1, (dino_x, dino_y))
-            leg_swap = False
-        else:
-            screen.blit(imgDino2, (dino_x, dino_y))
-            leg_swap = True
-
+        dino.draw(screen)
+        dino.dinoupdate(userinput)
         # update
         pygame.display.update()
         fps.tick(30)
